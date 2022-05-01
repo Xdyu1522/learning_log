ba206d6e999054f2ab58df5088fa53c96324a39a
Xie1522 <xdyu.e@outlook.com>
2022-05-01 15:00:06 +0800

    完成用户ç系统

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('topics/<int:topic_id>', views.topic, name='topic'),
     #新页面
     path('new_topic/', views.new_topic, name='new_topic'), 
+    #创建条目
+    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
     #编辑条目
-    path('new_entry/<int:topic_id>', views.new_entry, name='new_entry')
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry')
 ]
